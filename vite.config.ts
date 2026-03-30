b5c10660bf9821da591f32a75901170ffde948d2
salah-spec7 <salahansari077@gmail.com>
2026-03-31 00:26:55 +0530

    fix: remove aliante from all paths to use root paths for cleaner URLs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/aliante/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
